2e1440a729af0bf88c90f78624888af216b55edc
jo930 <jo930@users.noreply.github.com>
2015-12-05 21:30:29 +0100

    Hott Telemetry changes to correct wrong altitude and add climbrate

--- src/main/telemetry/hott.c
+++ src/main/telemetry/hott.c
@@ -201,10 +201,16 @@ void hottPrepareGPSResponse(HOTT_GPS_MSG_t *hottGPSMessage)
     hottGPSMessage->home_distance_L = GPS_distanceToHome & 0x00FF;
     hottGPSMessage->home_distance_H = GPS_distanceToHome >> 8;
 
-    uint16_t hottGpsAltitude = (GPS_altitude / 10) + HOTT_GPS_ALTITUDE_OFFSET; // 1 / 0.1f == 10, GPS_altitude of 1 == 0.1m
-
+    uint16_t hottGpsAltitude = GPS_altitude + HOTT_GPS_ALTITUDE_OFFSET; // CHANGED: removed / 10 for correct altitude in Remote
     hottGPSMessage->altitude_L = hottGpsAltitude & 0x00FF;
     hottGPSMessage->altitude_H = hottGpsAltitude >> 8;
+    
+    uint16_t hottGpsClimbrate = (GPS_altitude + HOTT_GPS_CLIMBRATE_OFFSET) / 100;
+    hottGPSMessage->climbrate_L = hottGpsClimbrate & 0x00FF;
+    hottGPSMessage->climbrate_H = hottGpsClimbrate >> 8;
+    
+    uint16_t hottGpsClimbrate3s = GPS_altitude + HOTT_GPS_CLIMBRATE_OFFSET;
+    hottGPSMessage->climbrate3s = hottGpsClimbrate3s;
 
     hottGPSMessage->home_direction = GPS_directionToHome;
 }
